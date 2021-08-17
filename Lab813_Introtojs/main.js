26bd4c31e0ea468e4f54635046069807cf20848a
connorherson <804120@seq.org>
2021-08-17 12:14:30 -0700

    update

--- Lab813_Introtojs/main.js
+++ Lab813_Introtojs/main.js
@@ -11,17 +11,17 @@ function init(){
     // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
     context = canvas.getContext("2d");
 
-    x = 31;
-    y = 31;    // initial x,y canvas location
-    dx = dy = 8;   // velocity in x and y directions
+    x = 46;
+    y = 46;    // initial x,y canvas location
+    dx = dy = 12;   // velocity in x and y directions
     a = 250;
     b = 300;
-    da = -2;
-    db = 2;
+    da = -3;
+    db = 5;
     p = 400;
     q = 30;
-    dp = -8;
-    dq = 15;
+    dp = -30;
+    dq = 16;
     animate();      // kick off the animation
 
 }
@@ -31,23 +31,23 @@ function animate() {
     // erase the HTMLCanvasElement
     context.clearRect(0,0,canvas.width,canvas.height);
     update();   // update location
-    circle(30, x, y, "black", "red");     // render
+    circle(45, x, y, "black", "red");     // render
     circle(80, a, b, "black", "orange");
     circle(15, p, q, "black", "blue");
     requestAnimationFrame(animate); // next cycle
-    if(x<30||x>770){
+    if(x<45||x>1255){
       dx*=-1;
     }
-    if(y<30||y>570){
+    if(y<45||y>555){
       dy*=-1;
     }
-    if(a<80||a>720){
+    if(a<80||a>1220){
       da*=-1;
     }
     if(b<80||b>520){
       db*=-1;
     }
-    if(p<15||p>785){
+    if(p<15||p>1285){
       dp*=-1;
     }
     if(q<15||q>585){

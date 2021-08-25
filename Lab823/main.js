90d548422ee4d1ce933e1c935900942d40b9da6b
connorherson <804120@seq.org>
2021-08-25 11:34:13 -0700

    Update main.js

--- Lab823/main.js
+++ Lab823/main.js
@@ -40,7 +40,8 @@ function init(){// https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasEl
     context = canvas.getContext("2d");
     b1 = [];
     for(let i = 0; i<15;i++){
-      b1.push(new Ball(Math.floor(Math.random()*(canvas.width-40)+20), Math.floor(Math.random()*(canvas.height-40)+20), Math.floor(10*(Math.random()-0.5)), Math.floor(10*(Math.random()-0.5)), 20, "blue"));
+      let radius = Math.floor(Math.random()*30+20)
+      b1.push(new Ball(Math.floor(Math.random()*(canvas.width-2*radius)+radius), Math.floor(Math.random()*(canvas.height-2*radius)+radius), Math.floor(10*(Math.random()-0.5)), Math.floor(10*(Math.random()-0.5)), radius, "blue"));
       if(b1[i].dx==0&&b1.dy==0){
         b1[i].dx=1;
         b1[i].dy=1;
@@ -54,7 +55,7 @@ function animate() {
       b1[i].run();
       isColliding = false;
       for(var j=i+1;j<b1.length;j++){
-        if(Math.sqrt(Math.pow((b1[i].x-b1[j].x), 2)+Math.pow((b1[i].y-b1[j].y), 2))<40){
+        if(Math.sqrt(Math.pow((b1[i].x-b1[j].x), 2)+Math.pow((b1[i].y-b1[j].y), 2))<(b1[i].radius+b1[j].radius)){
           b1[i].color = "orange";
           b1[j].color = "orange";
           isColliding = true;

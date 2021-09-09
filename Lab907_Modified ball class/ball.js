b4966ab2a17a84592ea0c3f3ce6335a8211291c5
connorherson <804120@seq.org>
2021-09-09 11:27:07 -0700

    made lab 907

---
+++ Lab907_Modified ball class/ball.js
@@ -0,0 +1,33 @@
+function Ball(position, velocity, radius, color, ctx){
+  this.position = new JSVector(position.x, position.y);
+  this.velocity = new JSVector(velocity.x, velocity.y);
+  this.radius = radius;
+  this.color=color;
+  this.context = ctx;
+}
+Ball.prototype.checkedges = function(){
+  if(this.position.x<this.radius||this.position.x>canvas.width-this.radius){
+    this.velocity.x*=-1;
+  }
+  if(this.position.y<this.radius||this.position.y>canvas.height-this.radius){
+    this.velocity.y*=-1;
+  }
+}
+Ball.prototype.update = function(){
+  this.position.x+=this.velocity.x;
+  this.position.y+=this.velocity.y;
+}
+Ball.prototype.draw = function(){
+  this.context.beginPath();
+  this.context.arc(this.position.x, this.y, this.radius, 0, 2*Math.PI);
+  this.context.strokeStyle = "black";
+  this.context.stroke();
+  this.context.fillStyle = this.color;
+  this.context.fill();
+}
+Ball.prototype.run = function(){
+  this.checkedges();
+  this.update();
+  this.draw();
+  console.log("running");
+}

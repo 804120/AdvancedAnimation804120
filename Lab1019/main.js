df102452754877624e737303477c66e71412459c
connorherson <804120@seq.org>
2021-10-19 13:34:57 -0700

    changed colors

--- Lab1019/main.js
+++ Lab1019/main.js
@@ -2,13 +2,17 @@ window.addEventListener("load", init);// wait for the page to finish loading wit
 document.addEventListener("click", generateParticleSystem);
 var canvas, context, pSystem;// global variables
 function init(){// https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement
+
     canvas = document.getElementById("cnv"); // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
     context = canvas.getContext("2d");
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas.width, canvas.height);
     pSystem = [];
     animate();      // kick off the animation
 }
 function animate() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "rgb("+0+","+0+","+0+","+0.3+")";
+    context.fillRect(0, 0, canvas.width, canvas.height);
     for(let i=0;i<pSystem.length;i++){
       pSystem[i].run();
     }

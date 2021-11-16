60eab4e5a0490dd1dd91b4ba6190f3c56d93a3b3
connorherson <804120@seq.org>
2021-11-16 13:38:29 -0800

    reorganized

--- Lab1101/main.js
+++ Lab1101/main.js
@@ -1,19 +1,6 @@
 window.addEventListener("load", init);// wait for the page to finish loading with init as the callback
-document.addEventListener('keydown', event => { //listens for the keydown event
-  if (event.code === 'ArrowUp') up = true; //sets the up condition to "true", which will change the animate function
-  else if (event.code === 'ArrowDown') down = true;//sets the down condition to "true", which will change the animate function
-  else if (event.code === 'ArrowRight') right = true;//sets the right condition to "true", which will change the animate function
-  else if (event.code === 'ArrowLeft') left = true;//sets the left condition to "true", which will change the animate function
-})
-document.addEventListener('keyup', event => { // when arrow keys are released, the screen will stop moving
-  if (event.code === 'ArrowUp') up = false;
-  if (event.code === 'ArrowDown') down = false;
-  if (event.code === 'ArrowRight') right = false;
-  if (event.code === 'ArrowLeft') left = false;
-})
 
-
-var canvas, context, world, pos, buffer, right, left, down, up, rect;// global variables
+var canvas, context, world;// global variables
 function init(){// https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement
     canvas = document.getElementById("cnv"); // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
     canvas2 = document.getElementById("cnv2"); //second canvas
@@ -21,56 +8,17 @@ function init(){// https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasEl
     ctx2 = canvas2.getContext("2d"); // second context
     context.fillStyle = "black";
     ctx2.fillStyle = "black";
-    rect = canvas2.getBoundingClientRect();
-    buffer = 75;
     context.fillRect(0, 0, canvas.width, canvas.height);
     ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
-    canvas2.addEventListener("click", move);
-    world = new World(new JSVector(2400, 1800), context, canvas); // this is another another another another comment
+    world = new World(new JSVector(2400, 1800), 75, context, canvas); // creating the world larger than the canvas
     ctx2.scale(canvas2.width/(world.width*2), canvas2.height/(world.height*2));
     animate();      // kick off the animation
 }
+
+
 function animate() {
-    context.fillStyle = "black";
     context.fillRect(0, 0, canvas.width, canvas.height);
-    ctx2.fillStyle = "black";
     ctx2.fillRect(0, 0, 2*world.width, 2*world.height);
     world.run();
-    renderSmallCanvas();
     requestAnimationFrame(animate); // next cycle
-}
-
-function renderSmallCanvas(){
-  ctx2.lineWidth = 5;
-  ctx2.beginPath();
-  ctx2.moveTo(world.width, 0);
-  ctx2.lineTo(world.width, 2*world.height);
-  ctx2.strokeStyle = "red";
-  ctx2.stroke();
-  ctx2.closePath();
-  ctx2.beginPath();
-  ctx2.moveTo(0, world.height);
-  ctx2.lineTo(2*world.width, world.height);
-  ctx2.stroke();
-  ctx2.closePath();
-  ctx2.beginPath();
-  ctx2.strokeStyle = "white";
-  ctx2.strokeRect(world.width+world.position.x-canvas.width/2, world.height+world.position.y-canvas.height/2, canvas.width, canvas.height);
-  ctx2.stroke();
-  ctx2.closePath();
-}
-
-function move(){
-  let x = event.offsetX;
-  let y = event.offsetY;
-  x*= world.width*2/canvas2.width;
-  y*=world.height*2/canvas2.height;
-  x-= world.width;
-  y-= world.height;
-  let newpos = new JSVector(x, y);
-  world.destination = newpos;
-}
-
-function value_limit(val, min, max) {
-  return val < min ? min : (val > max ? max : val);
 }
--- Lab1101/world.js
+++ Lab1101/world.js
@@ -1,10 +1,38 @@
-function World(dimensions, ctx, canvas){
+function World(dimensions, buffer, ctx, canvas){
   this.width = dimensions.x/2; //Throughout this document I had to use half the width more than the actual width, so I found it easier to divide this by 2.
   this.height = dimensions.y/2; // see comment above
   this.context = ctx; // probably unnecessary, but it's there just in case
   this.position = new JSVector(0, 0); // initial position
   this.destination = this.position; // since it's not moving yet, the destination is the same as the position
   this.canvas = canvas; // also probably unnecessary
+  this.buffer = buffer;
+  this.up = false;
+  this.down = false;
+  this.left = false;
+  this.right = false;
+  document.addEventListener('keydown', event => { //listens for the keydown event
+    if (event.code === 'ArrowUp') this.up = true; //sets the up condition to "true", which will change the animate function
+    else if (event.code === 'ArrowDown') this.down = true;//sets the down condition to "true", which will change the animate function
+    else if (event.code === 'ArrowRight') this.right = true;//sets the right condition to "true", which will change the animate function
+    else if (event.code === 'ArrowLeft') this.left = true;//sets the left condition to "true", which will change the animate function
+  })
+  document.addEventListener('keyup', event => { // when arrow keys are released, the screen will stop moving
+    if (event.code === 'ArrowUp') this.up = false;
+    if (event.code === 'ArrowDown') this.down = false;
+    if (event.code === 'ArrowRight') this.right = false;
+    if (event.code === 'ArrowLeft') this.left = false;
+  })
+
+  canvas2.addEventListener("click", event => { // move to a point on the smaller canvas when the user clicks there
+    let x = event.offsetX;
+    let y = event.offsetY;
+    x*= this.width*2/canvas2.width;
+    y*= this.height*2/canvas2.height;
+    x-= this.width;
+    y-= this.height;
+    let newpos = new JSVector(x, y);
+    this.destination = newpos;
+  });
 }
 World.prototype.draw = function(){
   this.context.save();
@@ -28,24 +56,50 @@ World.prototype.draw = function(){
   this.context.restore();
 }
 World.prototype.update = function(){
-  if(up){
+  if(this.up){
+    console.log("hi");
     this.destination.y=this.position.y-20;
   }
-  if(down){
+  if(this.down){
     this.destination.y=this.position.y+20;;
   }
-  if(left){
+  if(this.left){
     this.destination.x=this.position.x-20;;
   }
-  if(right){
+  if(this.right){
     this.destination.x=this.position.x+20;;
   }
-  this.destination.x = value_limit(this.destination.x, -1*(this.width+buffer-this.canvas.width/2), this.width+buffer-this.canvas.width/2);
-  this.destination.y = value_limit(this.destination.y, -1*(this.height+buffer-this.canvas.height/2), this.height+buffer-this.canvas.height/2);
+  this.destination.x = this.value_limit(this.destination.x, -1*(this.width+this.buffer-this.canvas.width/2), this.width+this.buffer-this.canvas.width/2);
+  this.destination.y = this.value_limit(this.destination.y, -1*(this.height+this.buffer-this.canvas.height/2), this.height+this.buffer-this.canvas.height/2);
   this.position.add(JSVector.subGetNew(this.destination, this.position).divide(3));
+  if(JSVector.subGetNew(this.destination, this.position).getMagnitude()<1) this.destination = this.position;
 
 }
 World.prototype.run = function(){
   this.update();
   this.draw();
+  this.runSmallCanvas();
+}
+
+World.prototype.runSmallCanvas = function(){
+    ctx2.lineWidth = 5;
+    ctx2.beginPath();
+    ctx2.moveTo(this.width, 0);
+    ctx2.lineTo(this.width, 2*this.height);
+    ctx2.strokeStyle = "red";
+    ctx2.stroke();
+    ctx2.closePath();
+    ctx2.beginPath();
+    ctx2.moveTo(0, this.height);
+    ctx2.lineTo(2*this.width, this.height);
+    ctx2.stroke();
+    ctx2.closePath();
+    ctx2.beginPath();
+    ctx2.strokeStyle = "white";
+    ctx2.strokeRect(this.width+this.position.x-canvas.width/2, this.height+this.position.y-canvas.height/2, canvas.width, canvas.height);
+    ctx2.stroke();
+    ctx2.closePath();
+}
+World.prototype.value_limit = function(val, min, max) {
+  return val < min ? min : (val > max ? max : val);
 }
